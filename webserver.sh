cddf9657f961371cf0414d65ca5a6644e87c48cb
manishamanu12345 <121796027+manishamanu12345@users.noreply.github.com>
2023-02-18 01:21:06 +0530

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -2,10 +2,17 @@
 # package updates
 sudo yum check-update
 sudo yum update
+sudo yum -y install apache2
+sudo yum -y install git
 sudo yum -y install httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo systemctl status httpd
+cd basicshell
+mkdir shell
+cd shell
+touch shell.txt
+ls
 # creating the html landing page
 cd /var/www/html/
 echo '<!DOCTYPE html>' > index.html
@@ -15,7 +22,7 @@ echo '<title>Level It Up</title>' >> index.html
 echo '<meta charset="UTF-8">' >> index.html
 echo '</head>' >> index.html
 echo '<body>' >> index.html
-echo '<h1>Welcome to Level Up in Tech</h1>' >> index.html
-echo '<h3>Red Team</h3>' >> index.html
+echo '<h1>Welcome to Shell Scripting.sh</h1>' >> index.html
+echo '<h3>Hello World</h3>' >> index.html
 echo '</body>' >> index.html
 echo '</html>' >> index.html
